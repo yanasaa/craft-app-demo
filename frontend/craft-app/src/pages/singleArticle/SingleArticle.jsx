fa5aca28d192d46de3d246dc1c9415cc966baf0f
yanasaa <yanasaa@gmail.com>
2024-01-16 16:21:25 +0300

    feat: single article structure

--- frontend/craft-app/src/pages/singleArticle/SingleArticle.jsx
+++ frontend/craft-app/src/pages/singleArticle/SingleArticle.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./SingleArticle.scss";
+import { LikeTwoTone, ShareAltOutlined } from "@ant-design/icons";
 
 function SingleArticle() {
   const { slug } = useParams();
@@ -13,14 +14,41 @@ function SingleArticle() {
     };
     getArticle();
   }, [slug]);
- 
+  console.log(article);
   return (
-    <section className="articlePage">
+    <section className="article">
       <div className="wrapper">
-        <h2>{article.title}</h2>
-        <h3>{article.id}</h3>
-        <h3>{article.author_full_name}</h3>
-        <p>{article.body}</p>
+        <div className="article__wrapper">
+          <h2 className="article__title">{article.title}</h2>
+          <div className="article__info">
+            <p className="article__author">{article.author_full_name}</p>
+            <p className="article__date">{article.publish}</p>
+          </div>
+        </div>
+        <div className="article__line"></div>
+        <div className="article__wrapper">
+          <div className="article__img">
+            <img src={article.preview} alt={article.title} />
+          </div>
+          <article className="article__text_full ">{article.body}</article>
+          <div className="article__feedback">
+            <div className="article__likes">
+              <LikeTwoTone
+                twoToneColor="#eb2f96"
+                className="likes__icon icon likes__icon_liked"
+              />
+              <span>{article.total_likes}</span>
+            </div>
+            <div className="article__share">
+              <ShareAltOutlined
+                className="share__icon"
+                style={{ fontSize: "26px", color: "#ad2e95" }}
+              />
+              <p className="share__text">Поделиться</p>
+            </div>
+          </div>
+        </div>
+        <div className="article__line"></div>
       </div>
     </section>
   );
