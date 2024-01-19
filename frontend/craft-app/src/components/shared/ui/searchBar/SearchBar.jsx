b2fc300a2b20568eb85d6ab095d16e65736bc508
yanasaa <yanasaa@gmail.com>
2024-01-19 18:01:03 +0300

    feat: search func

--- frontend/craft-app/src/components/shared/ui/searchBar/SearchBar.jsx
+++ frontend/craft-app/src/components/shared/ui/searchBar/SearchBar.jsx
@@ -4,16 +4,27 @@ import Input from "../input/Input";
 import "./SearchBar.scss";
 
 function SearchBar(props) {
-  const { className } = props;
-  const [isActive, setIsActive] = useState(false);
+  const { className, searchValue, setSearchValue } = props;
+  // const [isActive, setIsActive] = useState(false);
   return (
     <div className={className}>
-      {isActive && (
-        <Input className="search__input" type="search" placeholder="Поиск" focus={true}/>
+      {/* {isActive && ( */}
+      <input
+        className="search__input"
+        value={searchValue}
+        type="search"
+        placeholder="Поиск по автору или названию статьи"
+        onChange={(event) => {
+          setSearchValue(event.target.value);
+        }}
+      />
+      {searchValue && (
+        <span className="icon search-clear__btn" title="Очистить" onClick={() => setSearchValue('')}></span>
       )}
-      <div className="search__icons" onClick={() => setIsActive(!isActive)}>
+      <div className="search__icons">
         <SearchOutlined
           className="icon search__icon"
+          title="Найти"
           style={{ fontSize: "32px", color: "#ad2e95" }}
         />
       </div>
