91f47c4f540b65748919850c0a9d34ffb2583cde
yanasaa <yanasaa@gmail.com>
2024-01-24 19:21:25 +0300

    fix: searchbar add scroll by Enter

--- frontend/craft-app/src/components/shared/ui/searchBar/SearchBar.jsx
+++ frontend/craft-app/src/components/shared/ui/searchBar/SearchBar.jsx
@@ -4,6 +4,11 @@ import "./SearchBar.scss";
 function SearchBar(props) {
   const { className, searchValue, setSearchValue } = props;
   // const [isActive, setIsActive] = useState(false);
+  function handleKeyPress(e) {
+    if (searchValue && e.key === "Enter") {
+      window.scrollTo(0, 2150);
+    }
+  }
   return (
     <div className={className}>
       {/* {isActive && ( */}
@@ -15,9 +20,14 @@ function SearchBar(props) {
         onChange={(event) => {
           setSearchValue(event.target.value);
         }}
+        onKeyDown={handleKeyPress}
       />
       {searchValue && (
-        <span className="icon search-clear__btn" title="Очистить" onClick={() => setSearchValue('')}></span>
+        <span
+          className="icon search-clear__btn"
+          title="Очистить"
+          onClick={() => setSearchValue("")}
+        ></span>
       )}
       <div className="search__icons">
         <SearchOutlined
