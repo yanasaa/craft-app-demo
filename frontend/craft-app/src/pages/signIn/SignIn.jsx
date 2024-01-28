0bc3cff938ea4f53fa515fcfba44b2749d5fc9d5
yanasaa <yanasaa@gmail.com>
2024-01-28 22:17:47 +0300

    fix: refactor login page structure and add styles

--- frontend/craft-app/src/pages/signIn/SignIn.jsx
+++ frontend/craft-app/src/pages/signIn/SignIn.jsx
@@ -1,14 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import * as yup from "yup";
 import { Formik, Form, Field } from "formik";
-
 import { UserOutlined } from "@ant-design/icons";
-
 import Button from "../../components/shared/ui/button/Button";
-
 import "./Signin.scss";
 import { ROUTES } from "../../components/shared/consts/routes";
-import signin from "../../components/shared/assets/img/ui/signIn.png";
+
 import { useState } from "react";
 
 const END_POINT = "http://84.38.183.195/api/v1/account/login/";
@@ -20,7 +17,7 @@ const validationSchema = yup.object().shape({
 
 const SignIn = ({ setIsLoggedIn }) => {
   let navigate = useNavigate();
-  const [errorMessage, setErrorMessage] = useState("123");
+  const [errorMessage, setErrorMessage] = useState("");
 
   function handleLogin(values) {
     console.log(values);
@@ -56,68 +53,83 @@ const SignIn = ({ setIsLoggedIn }) => {
   }
 
   return (
-    <section className="sign-in__wrap">
-      <div className="sign-in__image">
-        <img src={signin} alt="signin" />
-      </div>
-      <div className="sign-in__form">
-        <h2>Мы рады вас видеть!</h2>
-        <Formik
-          className="login-form"
-          validationSchema={validationSchema}
-          initialValues={{ username: "", password: "" }}
-          onSubmit={(values) => {
-            handleLogin(values);
-          }}
-        >
-          {({ errors, touched }) => (
-            <Form
-              name="username"
-              label="Имя пользователя"
-              rules={[
-                {
-                  type: "username",
-                  message: "The input is not valid Login!",
-                },
-                {
-                  required: true,
-                  message: "Введите Ваш логин!",
-                },
-              ]}
-            >
-              {/* <Input
-                placeholder="hello"
-                prefix={<UserOutlined className="site-form-item-icon" />}
-              /> */}
-              <label>Имя пользователя</label>
-              <Field
-                prefix={<UserOutlined className="site-form-item-icon" />}
-                placeholder="Имя пользователя"
+    <section className="sign-in">
+      <div className=" wrapper sign-in__wrap">
+        <div className="sign-in__preview">
+          <h2>У нас много новинок</h2>
+          <h2 className="sign-in__preview_text">
+            Вдохновляйся работами мастеров.
+            <br />
+            Участики нашего комьюнити делятся полезной информацией и
+            представляют свои работы.
+          </h2>
+        </div>
+        <div className="sign-in__form">
+          <h2>Мы рады вас видеть!</h2>
+          <Formik
+            validationSchema={validationSchema}
+            initialValues={{ username: "", password: "" }}
+            onSubmit={(values) => {
+              handleLogin(values);
+            }}
+          >
+            {({ errors, touched }) => (
+              <Form
+                className="sif__wrapper"
                 name="username"
-              />
-              {errors.email && touched.email && <div>{errors.email}</div>}
+                label="Имя пользователя"
+                rules={[
+                  {
+                    type: "username",
+                    message: "The input is not valid Login!",
+                  },
+                  {
+                    required: true,
+                    message: "Введите Ваш логин!",
+                  },
+                ]}
+              >
+                {errorMessage && (
+                  <div className="sif__error-msg">{errorMessage}</div>
+                )}
+                <div className="sif__register-link">
+                  <span>Еще нет аккаунта на Craftshare?</span>
+                  <a href="#" title="Регистрация пока недоступна :(">
+                    Зарегистрироваться
+                  </a>
+                </div>
+                <label>
+                  Логин:
+                  <Field
+                    className="input sif__input"
+                    prefix={<UserOutlined className="site-form-item-icon" />}
+                    placeholder="Введите имя пользователя"
+                    name="username"
+                  />
+                </label>
+                {errors.email && touched.email && <div>{errors.email}</div>}
+                <label>
+                  Пароль:
+                  <Field
+                    className="input sif__input"
+                    placeholder="Введите пароль"
+                    name="password"
+                    type="password"
+                  />
+                </label>
+                {errors.password && touched.password && (
+                  <div>{errors.password}</div>
+                )}
 
-              <div padding={"10px"}>{errorMessage}</div>
-
-              <label>Пароль</label>
-              <Field
-                // className="input"
-                placeholder="Пароль"
-                name="password"
-                type="password"
-              />
-              {errors.password && touched.password && (
-                <div>{errors.password}</div>
-              )}
-
-              <Button
-                className="button button_colored"
-                btnText="Войти"
-                type="submit"
-              />
-            </Form>
-          )}
-        </Formik>
+                <Button
+                  className="button button_colored sif__button"
+                  btnText="Войти"
+                  type="submit"
+                />
+              </Form>
+            )}
+          </Formik>
+        </div>
       </div>
     </section>
   );
