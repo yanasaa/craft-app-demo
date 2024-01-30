ee808eb78f15c539bcd978db83abccc8df190541
yanasaa <yanasaa@gmail.com>
2024-01-30 15:36:52 +0300

    fix: create article text, profile cards, delete

---
+++ frontend/craft-app/src/pages/profile/ui/userArticleCard/UserArticleCard.jsx
@@ -0,0 +1,72 @@
+import { useNavigate } from "react-router-dom";
+import { LikeTwoTone, EditOutlined, DeleteOutlined } from "@ant-design/icons";
+
+import "./UserArticleCard.scss";
+import { ROUTES } from "../../../../components/shared/consts/routes";
+
+function UserArticleCard(props) {
+  const { className, title, body, slug, likes, imgSrc } = props;
+  let navigate = useNavigate();
+  function handleClick() {
+    navigate(`${ROUTES.MAIN}${slug}`);
+    window.scrollTo(0, 0);
+  }
+
+  function deleteArticle() {
+    const res = fetch(`http://84.38.183.195/api/v1/post/${slug}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(),
+    })
+      .then((response) => response.json())
+      .catch((error) => {
+        console.log(error);
+      });
+    
+  }
+
+  return (
+    <div className={className}>
+      <div className="card">
+        <div className="article-preview__image" onClick={handleClick}>
+          <img
+            src={
+              imgSrc
+                ? imgSrc
+                : "https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?q=65&auto=format&w=2270&ar=2:1&fit=crop"
+            }
+            alt={title}
+          />
+        </div>
+        <div className="card-body" onClick={handleClick}>
+          <h3 className="article-preview__title">{title}</h3>
+          {/* <p className="article-preview__text" dangerouslySetInnerHTML={{ __html: body }}></p> */}
+          <p className="article-preview__text" title={body}>
+            {body}
+          </p>
+        </div>
+        <div className="article-preview__info">
+          <div className="likes">
+            <LikeTwoTone twoToneColor="#ad2e95" className="likes__icon icon" />
+            <span>{likes}</span>
+          </div>
+          <div className="card__actions">
+            <EditOutlined
+              className="icon__user-card_action"
+              title="редактировать"
+            />
+            <DeleteOutlined
+              onClick={deleteArticle}
+              className="icon__user-card_action"
+              title="удалить"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserArticleCard;
